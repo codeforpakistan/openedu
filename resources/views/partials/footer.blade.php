2e7bfd28241d1c29561679b464437ce22d6b783a
Shaji Ahmed <schajee@gmail.com>
2016-07-19 12:26:31 +0500

    Added front assets

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="container">
+        <hr>
+        <ul>
+            <li><a href="/help/license">&copy 2016</a></li>
+            <li><a href="/help/about">About</a></li>
+            <li><a href="/help/terms">Terms</a></li>
+            <li><a href="/help/privacy">Privacy</a></li>
+            <li><a href="/help/contact">Contact</a></li>
+            <li><a href="/help">Help</a></li>
+        </ul>
+    </div>
+</footer>
+</body>
+</html>
